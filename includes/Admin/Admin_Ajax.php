d4e2abcfcf96b970c6a7e8e2d11c907b783266f8
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2021-01-12 13:34:31 +0600

    Update Admin_Ajax.php
    field sanitize updated

--- includes/Admin/Admin_Ajax.php
+++ includes/Admin/Admin_Ajax.php
@@ -11,25 +11,34 @@ class Admin_Ajax
   }
   public function register()
   {
-    add_action('wp_ajax_bit_email_log', array($this, 'getMailLog'));
     add_action('wp_ajax_bit_save_email_config', array($this, 'saveMailConfig'));
     add_action('wp_ajax_bit_send_test_email', array($this, 'sendTestEmail'));
     add_action('wp_ajax_bitforms_get_mail_config', array($this, 'getMailConfig'));
   }
-  public function getMailLog()
-  {
-    echo 'log';
+
+  private function mailPostData($request){
+    $mail_post_data = [
+      'status' => isset($request['status']) ? sanitize_text_field($request['status']) : '',
+      'form_email_address' => isset($request['form_email_address']) ? sanitize_email($request['form_email_address']) : '',
+      'form_name' => isset($request['form_name']) ? sanitize_text_field($request['form_name']) : '',
+      're_email_address' => isset($request['re_email_address']) ? sanitize_email($request['re_email_address']) : '',
+      'smtp_host' => isset($request['smtp_host']) ? sanitize_text_field($request['smtp_host']) : '',
+      'encryption' => isset($request['encryption']) ? sanitize_text_field($request['encryption']) : '',
+      'port' => isset($request['port']) ? sanitize_text_field($request['port']) : '',
+      'smtp_auth' => isset($request['smtp_auth']) ? sanitize_text_field($request['smtp_auth']) : '',
+      'smtp_user_name' => isset($request['smtp_user_name']) ? sanitize_text_field($request['smtp_user_name']) : '',
+      'smtp_password' => isset($request['smtp_password']) ? sanitize_text_field($request['smtp_password']) : ''
+    ];
+    return $mail_post_data;
   }
 
   public function saveMailConfig()
   {
-    if (wp_verify_nonce(sanitize_text_field($_REQUEST['_ajax_nonce']), 'bit_wp_smtp')) {
+    \ignore_user_abort();
+    if (wp_verify_nonce(sanitize_text_field($_REQUEST['_ajax_nonce']), 'bit_smtp')) {
       unset($_REQUEST['_ajax_nonce'], $_REQUEST['action']);
-      $mailDetails = json_encode($_REQUEST);
-    
-      $query = "UPDATE `{$this->wpdb->prefix}options` SET `option_value` = '$mailDetails' WHERE `{$this->wpdb->prefix}options`.`option_name`='bit_smtp_options'";
-   
-      $result = $this->wpdb->query($query);
+      $mail_post_data = $this->mailPostData($_REQUEST);
+      $result = update_option('bit_smtp_options',wp_json_encode($mail_post_data));
       wp_send_json_success($result, 200);
     } else {
       wp_send_json_error(
@@ -44,13 +53,13 @@ class Admin_Ajax
 
   public function sendTestEmail()
   {
-    if (wp_verify_nonce(sanitize_text_field($_REQUEST['_ajax_nonce']), 'bit_wp_smtp')) {
+    \ignore_user_abort();
+    if (wp_verify_nonce(sanitize_text_field($_REQUEST['_ajax_nonce']), 'bit_smtp')) {
       $inputJSON = file_get_contents('php://input');
       $queryParams = json_decode($inputJSON);
-
-      $to = wp_unslash($queryParams->to);
-      $subject = wp_unslash($queryParams->subject);
-      $message = wp_unslash($queryParams->message);
+      $to =  isset($queryParams->to) ? sanitize_email($queryParams->to) : '';
+      $subject =  isset($queryParams->subject) ? sanitize_text_field($queryParams->subject) : '';
+      $message  =  isset($queryParams->message) ? sanitize_text_field($queryParams->message) : '';
       unset($_REQUEST['_ajax_nonce'], $_REQUEST['action']);
       if (!empty($to) && is_email($to) && !empty($subject) && !empty($message)) {
         try {
@@ -60,13 +69,13 @@ class Admin_Ajax
           $status = $e->getMessage();
         }
       } else {
-        $status = __('Some of the test fields are empty or an invalid email supplied', 'wp-smtp');
+        $status = __('Some of the test fields are empty or an invalid email supplied', 'bit-smtp');
       }
     } else {
       wp_send_json_error(
         __(
           'Token expired',
-          'bitform'
+          'bitsmtp'
         ),
         401
       );
@@ -76,7 +85,6 @@ class Admin_Ajax
   public function getMailConfig()
   {
     $result = get_option('bit_smtp_options');
-
     wp_send_json_success($result, 200);
   }
 }
