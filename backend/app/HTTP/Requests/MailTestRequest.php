c47df6b8cc45a6a875b067054a16c2575de23ca9
csemazharul <csemazharulislam@gmail.com>
2024-03-03 15:17:51 +0600

    fix: custom message issue

--- backend/app/HTTP/Requests/MailTestRequest.php
+++ backend/app/HTTP/Requests/MailTestRequest.php
@@ -8,20 +8,22 @@ class MailTestRequest extends Request
 {
     public function rules()
     {
+        error_log(get_class($this));
+        error_log('Rules ');
         return
             [
-                'to'                      => ['required'],
-                'subject'                 => ['required'],
-                'message'                 => ['required'],
-            ];
+            'to' => ['required'],
+            'subject' => ['required'],
+            'message' => ['required'],
+        ];
     }
 
     public function messages()
     {
         return [
-            'to'                            => 'To Email Address is required',
-            'subject'                       => 'Subject is required',
-            'message'                       => 'Message is required',
+            'to.required' => 'To Email Address is required',
+            'subject.required' => 'Subject is required',
+            'message.required' => 'Message is required',
         ];
     }
 }
