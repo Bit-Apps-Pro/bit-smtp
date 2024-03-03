d8b60214a9d68a26b63bdaf5ea86aad65bafb126
csemazharul <csemazharulislam@gmail.com>
2024-03-03 15:17:38 +0600

    fix: nonce issue

--- backend/app/HTTP/Middleware/NonceCheckerMiddleware.php
+++ backend/app/HTTP/Middleware/NonceCheckerMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace BitApps\SMTP\HTTP\Middleware;
 
+use BitApps\SMTP\Config;
 use BitApps\WPKit\Http\Request\Request;
 use BitApps\WPKit\Http\Response;
 
@@ -9,8 +10,8 @@ final class NonceCheckerMiddleware
 {
     public function handle(Request $request, ...$params)
     {
-        if (! $request->has('_ajax_noce') || wp_verify_nonce(sanitize_key($request->_ajax_noce), 'bitdoc_noce')) {
-            // return Response::error('Invalid token')->httpStatus(411);
+        if (!$request->has('_ajax_nonce') || !wp_verify_nonce(sanitize_key($request->_ajax_nonce), Config::VAR_PREFIX . 'nonce')) {
+            return Response::error('Invalid token')->httpStatus(411);
         }
 
         return true;
