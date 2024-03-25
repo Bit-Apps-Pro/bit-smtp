ad512239abd9feb4258a33793ebcc1853ecd3d7a
Shakhawat Hosen <mdshakhawathosen122@gmail.com>
2024-03-25 12:01:52 +0600

    chore: sidebar logo addedd & description updated

--- bit_smtp.php
+++ bit_smtp.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BIT SMTP
  * Plugin URI:  https://www.bitapps.pro/bit-smtp
- * Description: Integrates with other platform
+ * Description: Send email via SMTP using BIT SMTP plugin by Bit Apps
  * Version:     1.0.9
  * Author:      Bit Apps
  * Author URI:  https://bitapps.pro
@@ -10,6 +10,6 @@
  * Requires PHP: 7.4
  * Requires WP: 5.0
  * Domain Path: /languages
- * License: gpl2+
+ * License: GPLv2 or later  
  */
 require_once plugin_dir_path(__FILE__) . 'backend/bootstrap.php';
